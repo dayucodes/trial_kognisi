f864a9c81ead2d5c4a74d956fd4700df2a911ad5
dayucodes <dahayubethari@gmail.com>
2026-05-04 14:07:54 +0700

    Update query_mykg_i.sql

--- query_mykg_i.sql
+++ query_mykg_i.sql
@@ -1,22 +1,31 @@
-SELECT  
-    u.email, 
-    u.name, 
-    '' AS 'nik',
-    CASE 
-        WHEN ubr.bundle_id = 1 THEN 'GI'
-        WHEN ubr.bundle_id = 2 THEN 'LEAN'
-        WHEN ubr.bundle_id = 3 THEN 'ELITE'
-        WHEN ubr.bundle_id = 4 THEN 'Genuine'
-        WHEN ubr.bundle_id = 5 THEN 'Astaka'
-    END AS 'title',
-    ubr.created_at AS 'last_updated',
-    1200 AS 'duration', 
-    100 AS 'progress',
-    'Assessment' AS 'type',
-    'Discovery' AS 'platform'
-FROM 
-    user_bundle_results ubr 
+
+SELECT 
+        '-' AS 'email',
+        i.name,
+        i.nik,
+        c.title,
+        c.created_at AS 'last_updated',
+        SUM(CASE 
+            WHEN c.type = 1 THEN a.duration 
+            WHEN c.type = 2 THEN TIME_TO_SEC(TIMEDIFF(cs2.end_time, cs2.start_time)) 
+        END) AS duration,  -- Sum up the duration
+	100 AS progress,
+        CASE WHEN c.type = 1 THEN 'Video'
+            WHEN c.type = 2 THEN 'Inclass' END AS 'type',
+        'Kognisi MyKG' AS 'platform'
+FROM
+		course_instructors ci 
 LEFT JOIN
-    users u ON u.id = ubr.user_id
-   ORDER BY
-ubr.created_at DESC
+		instructors i ON instructor_id = i.id
+LEFT JOIN
+		courses c ON ci.course_id = c.id 
+LEFT JOIN 
+		attachments a ON a.id = c.attachment_id
+LEFT JOIN 
+	course_schedules cs ON cs.course_id = c.id 
+LEFT JOIN 
+	course_sessions cs2 ON cs2.course_schedule_id = cs.id
+WHERE 
+        ci.deleted_at IS NULL
+        AND c.deleted_at IS NULL
+GROUP BY i.name, i.nik, c.title, c.type, c.created_at 
